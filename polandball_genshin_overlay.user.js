e675a549da2bf45dc5c497b1f182341bb650154a
MagicalGirlDittoChan <102891432+MagicalGirlDittoChan@users.noreply.github.com>
2022-04-04 00:04:35 -0700

    change to gacha alliance version

--- polandball_genshin_overlay.user.js
+++ polandball_genshin_overlay.user.js
@@ -1,34 +1,42 @@
 // ==UserScript==
-// @name         Polandball+Genshin overlay
-// @namespace    https://reddit.com/r/polandball
-// @version      0.0.3
-// @description  This script made possibe by: https://github.com/r-PlaceTux/Overlay AND https://gitlab.com/alifurkany/overlayify
-// @author       r/polandball
+// @name         The Gacha Alliance r/place Script
+// @namespace    http://tampermonkey.net/
+// @version      5.1
+// @description  The GFL/Va-11 Peppy OGs - Gacha Alliance official userscript
 // @downloadURL  https://github.com/MagicalGirlDittoChan/genshin-polandball-place/raw/main/polandball_genshin_overlay.user.js
+// @author       soreikomori, LeftHandedBread and every single fellow weeb
 // @match        https://hot-potato.reddit.com/embed*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
 // @grant        none
-// @license      GPL-3.0
+// @license      MIT
 // ==/UserScript==
+
+// NOTE: This has been updated to the version used by the gacha alliance! Please see the authors for proper attribution. It is posted here for convenience only.
+
 if (window.top !== window.self) {
     window.addEventListener('load', () => {
-        document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
-            (function () {
-                const i = document.createElement("img");
-                i.src = "https://www.dropbox.com/s/0o6u1a0771qfd6d/overlay.png?raw=1";
-                i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px;";
-                console.log(i);
-                return i;
-            })())
-    }, false);
-    window.addEventListener('load', () => {
-        document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
-            (function () {
-                const i = document.createElement("img");
-                i.src = "https://raw.githubusercontent.com/MagicalGirlDittoChan/genshin-polandball-place/main/polandballplace2overlay.png";
-                i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px;";
-                console.log(i);
-                return i;
-            })())
+        // Load the image
+        const image = document.createElement("img");
+        image.src = "https://www.dropbox.com/s/jr4crnn654mjxwz/overlay.png?raw=1";
+        image.onload = () => {
+            image.style = `position: absolute; left: 0; top: 0; width: ${image.width/3}px; height: ${image.height/3}px; image-rendering: pixelated; z-index: 1`;
+        };
+      
+        // Add the image as overlay
+        const camera = document.querySelector("mona-lisa-embed").shadowRoot.querySelector("mona-lisa-camera");
+        const canvas = camera.querySelector("mona-lisa-canvas");
+        canvas.shadowRoot.querySelector('.container').appendChild(image);
+      
+        // Add a style to put a hole in the pixel preview (to see the current or desired color)
+        const waitForPreview = setInterval(() => {
+            const preview = camera.querySelector("mona-lisa-pixel-preview");
+            if (preview) {
+              clearInterval(waitForPreview);
+              const style = document.createElement('style')
+              style.innerHTML = '.pixel { clip-path: polygon(-20% -20%, -20% 120%, 37% 120%, 37% 37%, 62% 37%, 62% 62%, 37% 62%, 37% 120%, 120% 120%, 120% -20%); }'
+              preview.shadowRoot.appendChild(style);
+            }
+        }, 100);
     }, false);
 }
+
